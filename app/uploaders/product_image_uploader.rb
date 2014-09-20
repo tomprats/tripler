f3abc5f4784a3f62f06f01d8ac7283f08fa695ea
Tom Prats <tprats108@gmail.com>
2014-09-20 19:58:46 -0400

    Switch cloudinary out for AWS

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -1,14 +1,5 @@
 class ProductImageUploader < CarrierWave::Uploader::Base
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-  else
-    storage :file
-  end
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  process convert: "png"
+  storage :fog
 
   def store_dir
     "products/#{model.id}"
@@ -36,9 +27,9 @@ class ProductImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
