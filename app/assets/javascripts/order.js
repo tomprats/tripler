981b6db3489529e0b0f31c00ab0b2c650d303d18
Tom Prats <tprats108@gmail.com>
2015-09-02 22:07:14 -0400

    Update look of orders and errors

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   if($(".pages.jerky").length > 0) {
-    $(".product-quantity select").on("change", function() {
+    $(".product-quantity .quantity").on("change", function() {
       var quantity = 0;
       var total = 0;
-      $(".product-quantity select").each(function() {
+      $(".product-quantity .quantity").each(function() {
         var $this = $(this);
         quantity += parseInt($this.val());
         total += $this.val() * parseFloat($this.closest(".product-box").find(".price").text());
@@ -26,7 +26,7 @@ $(document).ready(function() {
         var $this = $(this);
         order["order_items_attributes"].push({
           product_id: $this.data("id"),
-          quantity: $this.find("select").val()
+          quantity: $this.find(".quantity").val()
         });
       });
       $.post("/order", { order: order }, function(data) {
